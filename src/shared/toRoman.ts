c969f4eae99b467fd50b7c051f3690ea7aa7618b
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-04-09 01:07:54 +0300

    century name for BCE

--- src/shared/toRoman.ts
+++ src/shared/toRoman.ts
@@ -1,13 +1,23 @@
 // taken form https://www.selftaughtjs.com/algorithm-sundays-converting-roman-numerals/
-export const toRoman = (num: number) => {
+export const numberToRoman = (value: number) => {
     let result = '';
     const decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     const roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
     for (let i = 0; i <= decimal.length; i++) {
-        while (num % decimal[i] < num) {
+        while (value % decimal[i] < value) {
             result += roman[i];
-            num -= decimal[i];
+            value -= decimal[i];
         }
     }
     return result;
+}
+
+export const centuryToRoman = (value: number) => {
+    const sign = Math.sign(value);
+    const num = Math.abs(value);
+    if (sign <= 0) {
+        return `${numberToRoman(num + 1)} BCE`
+    }
+
+    return numberToRoman(num);
 }
