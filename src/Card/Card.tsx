9e4fcf5208949a3a0ccad6b2ad4f12b5ef0db338
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-04-08 16:38:39 +0300

    moved to next.js

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -1,66 +1,17 @@
-import React, { Component } from 'react';
-import injectSheet from 'react-jss';
+import React from 'react';
 import { HistoryEntry } from '../data';
+import './Card.scss';
 
-const styles = (theme:any) => ({
-    Card: {
-        maxWidth: '20rem',
-        display: 'flex',
-        flexDirection: 'row-reverse',
-        background: 'white',
-        borderRadius: '0.25rem',
-        overflow: 'hidden',
-        boxShadow: '0 0 2px rgba(0, 0, 0, 0.2)',
-        margin: '1rem',
-        textAlign: 'left',
-        maxHeight: '128px',
-        '&:hover': {
-            zIndex: '999',
-            maxHeight: '',
-        }
-    },
-    Body: {
-        paddingLeft: '1rem'
-    },
-    Title: {
-        color: '#222',
-        margin: '0.25rem 0 0',
-        '& a, a:visited': {
-            color: '#222',
-            textDecoration: 'none'
-        }
-    },
-    Date: {
-        fontSize: '0.6rem',
-        color: '#666'
-    },
-    ShortDescription: {
-        paddingTop: '0.5rem',
-        fontSize: '0.8rem',
-        color: '#333',
-    },
-    ImgWrapper: {
-        width: '128px',
-        height: '128px',
 
-        '& img': {
-            maxWidth: '128px',
-            maxHeight: '128px'
-        }
-    }
-});
-
-const Card = ({ classes, entry }: { classes:any, entry: HistoryEntry }) => (
-    <div id={entry.name } className={classes.Card}>
-        <div className={classes.Body}>
-            <h3 className={classes.Title}><a href={'#' + entry.name }>{entry.title}</a></h3>
-            <div className={classes.Date}>{entry.date}</div>
-            <div className={classes.ShortDescription}>{entry.shortDescription}</div>
+export const Card = ({ entry }: { entry: HistoryEntry }) => (
+    <div id={entry.name} className="Card">
+        <div className="Card__Body">
+            <h3 className="Card__Title"><a href={'#' + entry.name }>{entry.title}</a></h3>
+            <div className="Card__Date">{entry.date}</div>
+            <div className="Card__ShortDescription">{entry.shortDescription}</div>
         </div>
         <div
-            className={classes.ImgWrapper}
+            className="Card__ImgWrapper"
         ><img src={entry.imageUrl} /></div>
     </div>
 );
-
-export default injectSheet(styles)(Card);
--- src/Search/Search.tsx
+++ src/Search/Search.tsx
@@ -1,37 +1,14 @@
 import React, { Component } from 'react';
-import injectSheet from 'react-jss';
+import './Search.scss';
 
-const styles = (theme:any)=>({
-    Input: {
-        width: '30rem',
-        borderRadius: '2px',
-        fontSize: '1.25rem',
-        lineHeight: '2rem',
-        verticalAlign: 'middle',
-        background: '#f8f8f8',
-        border: '2px solid #f8f8f8',
-        padding: '0 1rem',
-
-        '&:focus': {
-            outline: 'none',
-            boxShadow: '0 0 0.25rem rgba(0, 0, 0, 0.25)',
-            background: 'white',
-            borderColor: 'white'
-        }
-    }
-})
-
-class Search extends Component<any> {
+export class Search extends Component<any> {
   render() {
     return (
-        <div>
+        <div className="Search">
             <input
-                className={this.props.classes.Input}
                 placeholder="Search"
                 />
         </div>
     );
   }
 }
-
-export default injectSheet(styles)(Search);
