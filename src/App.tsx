ab80708739094e0792806b5f9926a6c1527dc75b
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-04-08 23:03:16 +0300

    added meta to the page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,23 @@ import { Card } from './Card/Card';
 import { Search } from './Search/Search';
 import { data } from './data';
 import { Scales } from './Scales/Scales';
+import Head from 'next/head';
 import './App.scss';
 
 class App extends Component<any, any> {
   render() {
     return (
       <div className="App">
+        <Head>
+            <meta charSet="UTF-8" />
+            <title>One Page History</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes" />
+            <base  href="/" />
+            {/* manifest.json provides metadata used when your web app is added to the
+            homescreen on Android. See https://developers.google.com/web/fundamentals/engage-and-retain/web-app-manifest/ */}
+            <link rel="manifest" href="/static/manifest.json" />
+            <link rel="shortcut icon" href="/static/favicon.ico"></link>
+        </Head>
         <header className="App__Header">
             <Search />
         </header>
