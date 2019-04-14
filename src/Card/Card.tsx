9bc033b24938344f8e8c4d39ff859cd97249c6a0
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-04-15 00:22:34 +0300

    updated all events to DateRanges

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -15,16 +15,24 @@ export const Card = ({ entry }: { entry: HistoryEntry }) => (
                 : entry.position.top
         }}
     >
+        { entry.dating &&
+            <div className="Card__Range" style={{
+                height: Math.max(1, entry.dating.to.year() - entry.dating.from.year()) * 1.5,
+                marginTop: -Math.max(1, entry.dating.to.year() - entry.dating.from.year()) * 1.5
+            }}></div>
+        }
+
         <div className="Card__Body">
             <h3 className="Card__Title"><a className="Card__TitleLink" href={'#' + entry.name }>{entry.title}</a> <a className="Card__WikiLink" target="_blank" href={'https://wikipedia.org/wiki/' + entry.name }>[W]</a></h3>
             <div className="Card__Date">{entry.subtitle}</div>
             <div className="Card__ShortDescription">{entry.shortDescription}</div>
+
+            <div
+                className="Card__ImgWrapper"
+            ><img
+                alt={entry.name + ' representation'}
+                src={entry.imageUrl}
+                /></div>
         </div>
-        <div
-            className="Card__ImgWrapper"
-        ><img
-            alt={entry.name + ' representation'}
-            src={entry.imageUrl}
-            /></div>
     </div>
 );
