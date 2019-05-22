e257fbd23932946f55d3578001544c0838038792
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-05-22 14:46:54 +0300

    two rows design placement implementation

--- src/shared/yearToTopPos.ts
+++
@@ -1,8 +0,0 @@
-import { Moment } from 'moment';
-import { CURRENT_YEAR } from './currentYear';
-import { SCALE } from './const';
-
-export const getTopPos = (date: number|Moment) =>
-    SCALE * (CURRENT_YEAR -
-            (typeof date == 'number' ? date : date.year())
-          )
