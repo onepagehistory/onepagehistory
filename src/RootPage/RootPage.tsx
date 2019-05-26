cc4f615e882b23580f17a5e91e89b4ad251e91e6
PLaymaker8 <playmaker8@ukr.net>
2019-05-26 20:13:16 +0300

    logo, cards, font updated

--- src/RootPage/RootPage.tsx
+++ src/RootPage/RootPage.tsx
@@ -4,6 +4,7 @@ import { ScalesContainer } from '../Scales/ScalesContainer';
 import { Route } from 'react-router-dom'
 import { Routes } from 'react-static'
 import './RootPage.css'
+import { NewLayout } from '../pages/new-layout-page'
 
 
 export const RootPage = (props) => {
@@ -20,6 +21,18 @@ export const RootPage = (props) => {
 
             <div className="RootPage__Contents">
                 <section className="RootPage__Body">
+                    <div className="logo">
+                        <svg width="60" height="28" viewBox="0 0 60 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M36.7236 0H41.5221L31.7704 27.5524H27.7072L20.7804 7.73944L13.7762 27.5524H9.713L0 0H5.06933L11.9574 20.5482L19.1164 0H22.6766L29.6808 20.703L36.7236 0Z" fill="white"/>
+                            <path d="M54.7611 0H59.7143V27.5524H54.7611V15.595H40.6366L42.249 11.5318H54.7611V0Z" fill="white"/>
+                        </svg>
+                        <p className="logo__name">
+                            World History <br/>
+                            Educational project
+                        </p>
+
+
+                    </div>
                     <div className="RootPage__Scales">
                         <ScalesContainer
                             data={historyData}
