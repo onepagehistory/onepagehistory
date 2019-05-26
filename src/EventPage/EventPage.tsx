c22904553813546762aa0f3dc032e7385ef58fd8
PLaymaker8 <playmaker8@ukr.net>
2019-05-26 21:12:06 +0300

    font-family added, event page style [not final version]

--- src/EventPage/EventPage.tsx
+++ src/EventPage/EventPage.tsx
@@ -15,17 +15,20 @@ export const EventPage = (props: { pageData: IPageData }) => {
                 <meta name="description" content={ entry.short } />
             </Head>
             <Link className="EventPage__Close" to="/">&times;</Link>
-            <h1>
-                { entry.title }
-                &nbsp;
-                <a
-                    target="_blank"
-                    href={ `https://github.com/onepagehistory/onepagehistory/tree/dev/src/data/entries/${ entry.name }.md` }
-                >edit</a>
-            </h1>
-            <h6>{entry.subtitle}</h6>
+            <div className="EventPage__head">
+                <h1 className="EventPage__title">
+                    { entry.title }
+                    &nbsp;
+                    <a
+                        target="_blank"
+                        href={ `https://github.com/onepagehistory/onepagehistory/tree/dev/src/data/entries/${ entry.name }.md` }
+                    >edit</a>
+                </h1>
+                <h3 className="EventPage__sub-title">{entry.subtitle}</h3>
+            </div>
+
             <EventDescription value={content}/>
-            <a target="_blank" href={ entry.wikiUrl }>[wikipedia]</a>
+            <a target="_blank" href={ entry.wikiUrl } className="wikilink">[wikipedia]</a>
         </div>
     )
 };
