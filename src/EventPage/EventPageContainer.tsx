057b84f2ef1febaa79ebe304f4e508eb75f44c6f
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-05-27 14:05:16 +0300

    fixed scrolling padding, added element vertical spacing

--- src/EventPage/EventPageContainer.tsx
+++ src/EventPage/EventPageContainer.tsx
@@ -2,16 +2,42 @@ import React, { useEffect } from 'react';
 import { useRouteData } from 'react-static'
 import { EventPage } from './EventPage';
 import { IPageData } from './IPageData';
+import { CARD_WIDTH, YEAR_LENGTH } from '../shared/const.js';
 
 
 export default ()=> {
     const pageData: IPageData = useRouteData();
 
+    /**
+     * Scroll element into view
+     * ____________________________
+     * |xxx|                |xxxxx|
+     * |xxx|                |xxxxx|
+     * |xxx|       OK       |xxxxx|
+     * |xxx|                |xxxxx|
+     * |3yr|                | 34% |
+     * */
     useEffect(() => {
         const windowWidth = window.innerWidth;
-        // 33% is the width of the sidepanel
-        const halfOfVisibleScreen = ((windowWidth * 0.77) / 2);
-        const scrollLeft = pageData.entry['cardPosition'].x - halfOfVisibleScreen;
+        const windowScrollX = window.scrollX;
+        const elementX = pageData.entry['cardPosition'].x;
+        const xDiff = elementX - windowScrollX;
+        // arbitrary margin from the left
+        const LEFT_MARGIN = YEAR_LENGTH * 3;
+        // 33% is the width of the sidepanel + a small margin
+        const RIGHT_MARGIN = windowWidth * 0.333 + CARD_WIDTH + 30;
+        if (xDiff > LEFT_MARGIN && xDiff < windowWidth - RIGHT_MARGIN) {
+            return;
+        }
+
+        let scrollLeft = 0;
+
+        if (xDiff <= LEFT_MARGIN) {
+            scrollLeft =  elementX - LEFT_MARGIN;
+        } else {
+            scrollLeft =  elementX - (windowWidth - RIGHT_MARGIN);
+        }
+
         window.scrollTo({ left: scrollLeft });
     });
 
--- src/Scales/ScalesContainer.tsx
+++ src/Scales/ScalesContainer.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Scales } from './Scales';
+import { plotChart } from '../data/plot-chart.js';
 
-export const ScalesContainer = ({ data, selectedId }) => {
+export const ScalesContainer = ({ data: rawData, selectedId }) => {
+    // TODO: do client-side plotting only in dev mode
+    const data = plotChart(rawData);
     return (
         <Scales
             selectedId={selectedId}
