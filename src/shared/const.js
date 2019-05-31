c8b0e3f69890e514ac5ea164b0971ee4b0ddc2c7
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-05-31 16:18:27 +0300

    using js sizes in styles

--- src/shared/const.js
+++ src/shared/const.js
@@ -1,8 +1,8 @@
 export const YEAR_LENGTH = 10;
-export const IMAGE_SIZE = 80;
+export const IMAGE_SIZE = 60;
 export const DESCRIPTION_WIDTH = 250;
 export const DESCRIPTION_MARGIN = 7;
 export const CARD_WIDTH = IMAGE_SIZE + DESCRIPTION_WIDTH + DESCRIPTION_MARGIN;
 export const CARD_HEIGHT = IMAGE_SIZE;
-export const STEP_HEIGHT = 16;
+export const STEP_HEIGHT = 10;
 export const CURRENT_YEAR = (new Date()).getFullYear() 
