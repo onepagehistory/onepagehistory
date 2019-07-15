87dd5050beabfc6c9ced8afe76e1314eee28f35e
PLaymaker8 <playmaker8@ukr.net>
2019-07-16 02:01:41 +0300

    updated logo with link to home (w/o scrolling), refactored rootpage in scope of naming-styling

--- src/RootPage/RootPage.tsx
+++ src/RootPage/RootPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSiteData } from 'react-static';
 import { ScalesContainer } from '../Scales/ScalesContainer';
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import { Routes } from 'react-static'
 import './RootPage.css'
 
@@ -13,14 +13,14 @@ export const RootPage = (props) => {
     const pageId = props.match.params.pageId;
 
     return (
-        <section className="RootPage">
+        <section className="root-page">
             {/* <React.Suspense fallback={<em>Loading...</em>}> */}
                 <Route path="/p/:id" render={() => <Routes path="/p/*"/> } />
             {/* </React.Suspense> */}
 
-            <div className="RootPage__Contents">
-                <section className="RootPage__Body">
-                    <div className="logo">
+            <div className="root-page__contents">
+                <section>
+                    <Link to="/" className="logo">
                         <svg width="60" height="28" viewBox="0 0 60 28" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M36.7236 0H41.5221L31.7704 27.5524H27.7072L20.7804 7.73944L13.7762 27.5524H9.713L0 0H5.06933L11.9574 20.5482L19.1164 0H22.6766L29.6808 20.703L36.7236 0Z" fill="white"/>
                             <path d="M54.7611 0H59.7143V27.5524H54.7611V15.595H40.6366L42.249 11.5318H54.7611V0Z" fill="white"/>
@@ -29,15 +29,11 @@ export const RootPage = (props) => {
                             World History <br/>
                             Educational project
                         </p>
-
-
-                    </div>
-                    <div className="RootPage__Scales">
-                        <ScalesContainer
+                    </Link>
+                    <ScalesContainer
                             data={historyData}
                             selectedId={pageId}
-                        />
-                    </div>
+                    />
 
                     {/* TODO: remove this when all rights will be resolved and before going public */}
                     {/* <div className="RootPage__Disclaimer">
