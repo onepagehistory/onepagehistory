10cd95694938b76772ab42b5b56055446fbd0146
PLaymaker8 <playmaker8@ukr.net>
2019-07-18 01:49:18 +0300

    logo click scroll function added

--- src/RootPage/RootPage.tsx
+++ src/RootPage/RootPage.tsx
@@ -12,6 +12,10 @@ export const RootPage = (props) => {
     // page id from the react-router url match
     const pageId = props.match.params.pageId;
 
+    function logoClick(){
+        return window.scrollTo(0,0);
+    }
+
     return (
         <section className="root-page">
             {/* <React.Suspense fallback={<em>Loading...</em>}> */}
@@ -20,7 +24,7 @@ export const RootPage = (props) => {
 
             <div className="root-page__contents">
                 <section>
-                    <Link to="/" className="logo">
+                    <Link to="/" className="logo" onClick={logoClick}>
                         <svg width="60" height="28" viewBox="0 0 60 28" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M36.7236 0H41.5221L31.7704 27.5524H27.7072L20.7804 7.73944L13.7762 27.5524H9.713L0 0H5.06933L11.9574 20.5482L19.1164 0H22.6766L29.6808 20.703L36.7236 0Z" fill="white"/>
                             <path d="M54.7611 0H59.7143V27.5524H54.7611V15.595H40.6366L42.249 11.5318H54.7611V0Z" fill="white"/>
