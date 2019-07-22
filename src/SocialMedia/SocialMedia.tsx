af8e49e1bad4c556dade0cc846dcd3a8237cb80a
Playmaker8 <playmaker8@ukr.net>
2019-07-23 01:01:06 +0300

    updated twitter sharing with current url, reverted logo click handler

--- src/SocialMedia/SocialMedia.tsx
+++ src/SocialMedia/SocialMedia.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import history from 'react-router'
-import './SocialMedia.css'
+import './SocialMedia.css';
 
-export class SocialMedia extends React.Component<{title: string, follow: boolean}> {
+export class SocialMedia extends React.Component<{title: string, follow: boolean}> {  
+    urlToShare: string;
+
+    componentWillMount(){
+        this.urlToShare = location.href;
+    }
+
+    componentWillUpdate(){
+        this.urlToShare = location.href;
+    }
 
     render() {
         return(
@@ -20,7 +28,7 @@ export class SocialMedia extends React.Component<{title: string, follow: boolean
                         </svg>
                     </a>
                 :
-                    <a target="_blank" href={`https://twitter.com/intent/tweet?text=Look+what+I+found+out+here&%3A%2F%2Furl=original_referer`}>
+                    <a target="_blank" rel="canonical" href={`https://twitter.com/intent/tweet?text=Look+what+I+found+out+here+${this.urlToShare}`}>
                         <svg className="Share-box__icon" width="21" height="18" viewBox="0 0 21 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M20.3333 2.79875C19.5895 3.125 18.797 3.34125 17.9708 3.44625C18.8208 2.93875 19.4695 2.14125 19.7745 1.18C18.982 1.6525 18.107 1.98625 17.1745 2.1725C16.422 1.37125 15.3495 0.875 14.1795 0.875C11.9095 0.875 10.082 2.7175 10.082 4.97625C10.082 5.30125 10.1095 5.61375 10.177 5.91125C6.76825 5.745 3.752 4.11125 1.72575 1.6225C1.372 2.23625 1.1645 2.93875 1.1645 3.695C1.1645 5.115 1.89575 6.37375 2.98575 7.1025C2.327 7.09 1.68075 6.89875 1.13325 6.5975C1.13325 6.61 1.13325 6.62625 1.13325 6.6425C1.13325 8.635 2.5545 10.29 4.41825 10.6712C4.0845 10.7625 3.72075 10.8062 3.34325 10.8062C3.08075 10.8062 2.81575 10.7913 2.567 10.7362C3.09825 12.36 4.60575 13.5538 6.39825 13.5925C5.00325 14.6838 3.232 15.3412 1.3145 15.3412C0.978252 15.3412 0.655752 15.3263 0.333252 15.285C2.1495 16.4563 4.302 17.125 6.62325 17.125C14.1683 17.125 18.2933 10.875 18.2933 5.4575C18.2933 5.27625 18.287 5.10125 18.2783 4.9275C19.092 4.35 19.7758 3.62875 20.3333 2.79875Z" fill="white"/>
                         </svg>
