735265c8226fe21bcbc06e49ebcb1f50d5bd3ca4
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2019-10-14 21:07:34 +0300

    basic decades for mobile

--- src/Mobile/MobilePageComponent.tsx
+++ src/Mobile/MobilePageComponent.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { CardLine } from '../CardLine/CardLine';
 import { useSiteData } from 'react-static';
 import { MobileMinimap } from './Minimap/MobileMinimap';
+import { MobileDecades } from './Decades/MobileDecades';
 import './MobilePageComponent.scss';
 
 export const MobilePageComponent = (props: any) => {
@@ -10,6 +11,9 @@ export const MobilePageComponent = (props: any) => {
 
     return <div className="mobile-page">
         <div className="mobile-page__card-line">
+            <div className="mobile-page__decades">
+                <MobileDecades />
+            </div>
             <CardLine
                 cards={cards}
                 />
