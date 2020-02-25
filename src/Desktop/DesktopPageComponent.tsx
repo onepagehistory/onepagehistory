fdb1187c9b821550456c42207b485156b36e94a3
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2020-02-25 23:37:17 +0200

    extracted logo click scroll fn

--- src/Desktop/DesktopPageComponent.tsx
+++ src/Desktop/DesktopPageComponent.tsx
@@ -6,6 +6,9 @@ import { SocialMedia } from '../SocialMedia/SocialMedia';
 import EventPageContainer from '../EventPage/EventPageContainer';
 import './DesktopPageComponent.scss';
 
+function resetPageScroll() {
+    return window.scrollTo(0, 0);
+}
 
 export const DesktopPageComponent = () => {
     const { data } = useSiteData();
@@ -13,10 +16,6 @@ export const DesktopPageComponent = () => {
     // page id from the page data
     const routeData = useRouteData();
     const pageId = routeData && routeData.entry && routeData.entry.name || void 0;
-
-    function logoClick(){
-        return window.scrollTo(0,0);
-    }
 
     return (
         <section className="root-page">
@@ -32,7 +31,7 @@ export const DesktopPageComponent = () => {
 
             <div className="root-page__contents">
                 <section>
-                    <Link to="/" className="logo" onClick={logoClick}>
+                    <Link to="/" className="logo" onClick={resetPageScroll}>
                         <svg width="60" height="28" viewBox="0 0 60 28" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M36.7236 0H41.5221L31.7704 27.5524H27.7072L20.7804 7.73944L13.7762 27.5524H9.713L0 0H5.06933L11.9574 20.5482L19.1164 0H22.6766L29.6808 20.703L36.7236 0Z" fill="white"/>
                             <path d="M54.7611 0H59.7143V27.5524H54.7611V15.595H40.6366L42.249 11.5318H54.7611V0Z" fill="white"/>
